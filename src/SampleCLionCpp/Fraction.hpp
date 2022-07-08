b4ea0203e900210c1def2642fbb03f8e53dcee6e
Oğuz Karan <oguzkaran@csystem.org>
2022-07-08 21:10:03 +0300

    Update

--- src/SampleCLionCpp/Fraction.hpp
+++ src/SampleCLionCpp/Fraction.hpp
@@ -10,7 +10,6 @@
     Copyleft (c) 1993 by C and System Programmers Association (CSD)
     All Rights Free
 ----------------------------------------------------------------------------------------------------------------------*/
-
 #ifndef FRACTION_HPP_
 #define FRACTION_HPP_
 
@@ -23,41 +22,54 @@ namespace org::csystem::math {
     private:
         int m_a, m_b;
     public:
-        Fraction(int a, int b);
-
+        Fraction();
+        explicit Fraction(int a, int b = 0);
     public:
+        //accessors
         int numerator() const;
-
         void numerator(int a);
-
         int denominator() const;
-
         void denominator(int a);
-
         double realValue() const;
-
     public:
+        //additive operators
         Fraction operator+(const Fraction &other) const;
         Fraction operator+(int value) const;
-        Fraction &operator+=(const Fraction &other);
-        Fraction &operator+=(int value);
+        Fraction &operator+=(const Fraction &other) const;
+        Fraction &operator+=(int value) const;
 
+        //subtractive operators
         Fraction operator-(const Fraction &other) const;
         Fraction operator-(int value) const;
-        Fraction &operator-=(const Fraction &other);
-        Fraction &operator-=(int value);
+        Fraction &operator-=(const Fraction &other) const;
+        Fraction &operator-=(int value) const;
 
+        //multiplicative operators
         Fraction operator*(const Fraction &other) const;
         Fraction operator*(int value) const;
-        Fraction &operator*=(const Fraction &other);
-        Fraction &operator*=(int value);
+        Fraction &operator*=(const Fraction &other) const;
+        Fraction &operator*=(int value) const;
 
+        //divide operators
         Fraction operator/(const Fraction &other) const;
         Fraction operator/(int value) const;
-        Fraction &operator/=(const Fraction &other);
-        Fraction &operator/=(int value);
+        Fraction &operator/=(const Fraction &other) const;
+        Fraction &operator/=(int value) const;
 
-        
+        //Sign - and + operators
+        Fraction operator-() const;
+        Fraction operator+() const;
+
+        //increment operators
+        Fraction &operator++();
+        Fraction operator++(int);
+
+        //decrement operators
+        Fraction &operator--();
+        Fraction operator--(int);
+
+        //explicit type cast to real value of fraction
+        explicit operator double() const;
     };
 }
 
