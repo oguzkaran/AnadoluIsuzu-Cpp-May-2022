6fc409b966049475d43bb4cf8fa5fcb792b4824d
Oğuz Karan <oguzkaran@csystem.org>
2022-07-29 11:30:13 +0300

    Update

--- src/SampleCLionCpp/main.cpp
+++ src/SampleCLionCpp/main.cpp
@@ -1,74 +1,28 @@
 /*----------------------------------------------------------------------------------------------------------------------
-    Bir referansın ve bir pointer'ın iki tane türü vardır: static, dynamic
-    - static type: Referansın ya da göstericinin bildirildiği ya da tanımlandığı türdür. Değişmez. Derleme zamanına
-    ilişkindir. Referansın ya da pointer'ın türü dendiğinde static tür anlaşılır.
+    Runtime polymorphism: Programlamaya Biyoloji'den aktarılmıştır. Biyoloji'de polymophism "farklı doku ya da organların
+    evrim süreci içerisinde temel işlevleri aynı kalmak koşuluyla, bu işlevi yerine getirme biçiminin değişebilmesidir"
+    şeklinde tanımlanabilir. Örneğin duyma davranışı birbirinden türemiş canlılar arasında
+    şeklinde tanımlanabilir. Örneğin duyma davranışı birbirinden türemiş canlılar arasında farklılık gösterebilir. Ancak
+    işlev (göre ya da hedef) "duymaktır".
 
-    - dynamic type: Bir referansın ya da göstericinin içerisinde bulunan adrese ilişkin bellekte yaratılmış olan gerçek
-    nesnenin türüdür. Değişebilir. Çalışma zamanına ilişkindir
+    Nesne yönelimli programlamada "polymorphism" dendiğinde "runtime polymıorphism" anlaşılır. Ayrıca "compile time polymorphism"
+    de vardır. Bu konu C++'da genel olarak "template" kullanılarak geçekleştirilir. İleride ele alınacaktır
+
+    Runtime polymorphism için çok fazla tanım, betimleme, açıklma yapılabilse de genel olarak aşğıdaki 3 tanıma
+    indirgenebilir:
+
+    1. Biyolojik Tanım: Taban sınıfın bir fonksiyonunun türemiş sınıfta yeniden gerçekleştirilmesi
+    2. Yazılım Mühendisliği Tanımı: Türden bağımsız kod yazmaktır
+    3. Aşağı seviyeli Tanım: Önceden yazılmış kodların sonradan yazılmış kodları çağırabilmesidir
 ----------------------------------------------------------------------------------------------------------------------*/
 #include <iostream>
-
-class A {
-    //...
-private:
-    int m_x;
-public:
-    A(int x) : m_x{x}
-    {}
-
-    A(const A&r) : m_x{r.m_x}
-    {
-        std::cout << "Copy ctor of A\n";
-    }
-
-    A &operator=(const A &r)
-    {
-        m_x = r.m_x;
-        std::cout << "Copy assignment operator function of A\n";
-
-        return *this;
-    }
-
-    int x() const {return m_x;}
-    void x(int value) {m_x = value;}
-    //...
-};
-
-
-class B : public A {
-private:
-    int m_y;
-public:
-    B(int x, int y) : A{x}, m_y{y}
-    {}
-    B(const B&r) : A{r}, m_y{r.m_y}
-    {
-        std::cout << "Copy ctor of B\n";
-    }
-
-    B &operator=(const B &r)
-    {
-        x(r.x());
-        m_y = r.m_y;
-        std::cout << "Copy assignment operator function of B\n";
-
-        return *this;
-    }
-
-    int y() const {return m_y;}
-    void y(int value) {m_y = value;}
-    //...
-};
 
 int main()
 {
     using namespace std;
 
-    B b{10, 20};
 
-    A *p = &b;
-    A &r = b;
+
 
     return 0;
 }
-
