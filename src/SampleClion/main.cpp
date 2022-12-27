64a7003a06de63f829f77a4d10a50ac430a5babf
Oğuz Karan <oguzkaran@csystem.org>
2022-12-28 01:34:12 +0300

    Update

--- src/SampleClion/main.cpp
+++ src/SampleClion/main.cpp
@@ -1,6 +1,20 @@
+/*----------------------------------------------------------------------------------------------------------------------
+    tuple_cat fonksiyonu ile pair ve tuple birleştirmesi yapılabilir. Bu fonksiyonlara yeni bir tupl elde edilir
+----------------------------------------------------------------------------------------------------------------------*/
 #include <iostream>
+#include <tuple>
+#include <utility>
+#include "csd/utility.hpp"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+#include <cstring>
+
+int main()
+{
+    using namespace std;
+
+
+
     return 0;
 }
+
+
