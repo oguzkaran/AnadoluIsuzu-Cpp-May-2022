2ec33e90d4f3233b7b112e6834c37638849d9a8b
Oğuz Karan <oguzkaran@csystem.org>
2023-01-13 12:50:20 +0300

    Update

--- src/SampleClion/main.cpp
+++ src/SampleClion/main.cpp
@@ -1,47 +1,68 @@
 /*----------------------------------------------------------------------------------------------------------------------
-    Mutex (mutual exclusion) bir kritik bölgeye yalnızca tek bir thread'in girebilmesini sağlayan senkronizasyon nesneleridir.
-    Mutex, C++'da mutex sınıfı ile temsil edilir. mutex sınıfı mutex header'i içerisinde bildirilmiştir. mutex ile
-    kritik bölge oluşturmak için ilgili thread'lerde kullanılabilecek bir mutex nesnesi yaratılır. kritik kod aşağıdaki
-    biçimde senkronize edilebilir:
-        g_mutex.lock();
-        <kritik bölge>
-        g_mnutex.unlock();
-    lock fonksiyonu çağrıldığında (mutex için lock yapıldığında) kritik koda giren thread mutex'in sahipliğini devralır
-    (got ownership of mutex)
-
-    Aşağıdaki örneği inceleyiniz
+    Yukarıdaki üretici tüketici probleminin kuyruk kullanılarak çözümü
 ----------------------------------------------------------------------------------------------------------------------*/
 #include <iostream>
 #include <thread>
-#include <vector>
-#include <mutex>
+#include <chrono>
+#include <semaphore>
+#include <array>
 #include "csd/utility.hpp"
 
-std::vector<int> g_values;
-std::mutex g_mutex;
+constexpr std::size_t queue_size = 10;
+std::array<std::size_t, queue_size> g_queue;
+std::size_t g_head, g_tail;
+std::counting_semaphore<queue_size> g_semProducer{queue_size}, g_semConsumer{0};
 
-int main()
+void producerThreadCallback(std::size_t n)
+{
+    using namespace std;
+    using org::csystem::util::random::randomInt;
+    size_t count{};
+
+    for (;;) {
+        g_semProducer.acquire();
+        g_queue[g_tail++] = count;
+        g_tail %= queue_size;
+        g_semConsumer.release();
+        ++count;
+        this_thread::sleep_for(chrono::microseconds(randomInt(0, 300000)));
+        if (count == 100)
+            break;
+    }
+}
+
+void consumerThreadCallback(std::size_t n)
 {
     using namespace std;
     using org::csystem::util::random::randomInt;
 
-    auto f = [](size_t n) {
-        for (size_t i{}; i < n; ++i) {
-            g_mutex.lock();
-            g_values.push_back(randomInt(0, 99));
-            g_mutex.unlock();
-        }
-    };
+    size_t val;
 
-    thread t1{f, 1000000};
-    thread t2{f, 2000000};
-    thread t3{f, 3000000};
+    for (;;) {
+        g_semConsumer.acquire();
+        val = g_queue[g_head++];
+        g_head %= queue_size;
+        g_semProducer.release();
+        cout << val << ' ';
+        cout.flush();
+
+        this_thread::sleep_for(chrono::microseconds(randomInt(0, 300000)));
+        if (val == 99)
+            break;
+    }
+
+    cout << '\n';
+}
+
+int main()
+{
+    using namespace std;
+
+    thread t1{producerThreadCallback, 10000};
+    thread t2{consumerThreadCallback, 20000};
 
     t1.join();
     t2.join();
-    t3.join();
-
-    cout << "Size = " << g_values.size() << '\n';
 
     return 0;
 }
